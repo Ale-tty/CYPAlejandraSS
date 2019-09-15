f9c0caa1b623512321f68ea4c919fbd69050440e
ale441 <alejandra.p.sanchez.s@gmail.com>
2019-09-15 01:11:54 -0500

    Agregue el ejemplo 1.11 del libro

---
+++ libro/ejemplo1_11.py
@@ -0,0 +1,10 @@
+# el programa dado un conjunto de datos de entrada invierte el orden de los mismos cuando los imprime 
+A = input ("Introduce un dato ")
+B = input ("Introduce otro dato ")
+C = input ("Introduce otro dato ")
+D = input ("Introduce otro dato ")
+print (D)
+print (C)
+print (B)
+print (A)
+
