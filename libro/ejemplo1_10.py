ec054d86ed8448ddbc09c994c56ab87f7d090b26
ale441 <alejandra.p.sanchez.s@gmail.com>
2019-09-15 00:50:12 -0500

    Agregue el archivo del ejemplo 1.10 del libro

---
+++ libro/ejemplo1_10.py
@@ -0,0 +1,6 @@
+BASE = int (input ("Dame la base de tu triangulo "))
+ALTU = int (input ("Dame la altura de tu triangulo "))
+SUP = (BASE * ALTU) / 2 
+PER = BASE * 3
+print (f"La superficie de tu triangulo es { SUP }")
+print (f"El perimetro de tu tringulo es { PER }")
