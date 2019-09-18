0512e2224d812f788a274d9dac96a448d1093825
ale441 <alejandra.p.sanchez.s@gmail.com>
2019-09-18 00:03:38 -0500

    Agregue el ejemplo1.15

---
+++ libro/ejemplo1_15.py
@@ -0,0 +1,5 @@
+BASE = int (input ("Introduce el valor de tu base del rectangulo "))
+ALTU = int (input ("Introduce el valor de tu altura de tu rectangulo "))
+SUP = BASE * ALTU 
+PER = 2 * (BASE + ALTU) 
+print (f"La superficie de tu rectangulo es { SUP } y el perimetro es { PER }")
