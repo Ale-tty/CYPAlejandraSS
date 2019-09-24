72d2c39ce192a0ac561fbc05e6c93f3cc6614775
ale441 <alejandra.p.sanchez.s@gmail.com>
2019-09-24 17:32:42 -0500

    Se agregaron los ejemplos de if

---
+++ mayor_de_3.py
@@ -0,0 +1,11 @@
+NUM1 = float (input ("Escribe un numero"))
+NUM2 = float (input ("Escribe un numero "))
+NUM3 = float (input ("Escibre un numero "))
+if NUM1 < NUM2 and NUM2 < NUM3: 
+    print (f"{NUM3} Es el numero mayor")
+if NUM1 > NUM2 and NUM2 > NUM3:
+        print (f"{NUM1} Es el numero mayor") 
+if NUM1 < NUM2 and NUM2 > NUM3:  
+        print (f"{NUM2} Es el numero mayor") 
+print ("Fin del programa") 
+
