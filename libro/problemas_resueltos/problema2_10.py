d9971f3ae260a9361440b4f4a8c332ec313bfbde
ale441 <alejandra.p.sanchez.s@gmail.com>
2019-10-10 17:01:30 -0500

    Se agrego el problema 2.10

---
+++ libro/problemas_resueltos/problema2_10.py
@@ -0,0 +1,30 @@
+A = int (input("Introduce un numero entero "))
+B= int (input("introduce un numero entero "))
+C= int (input("Introduce un numero entero ")) 
+if A > B: 
+    if  A > C: 
+        print (f"{A} Es el mayor")
+    else: 
+        if A == c: 
+            print (f"{A} y {C} son los mayores") 
+        else: 
+            print (f"{C} Es el mayor")
+else: 
+    if A == B :
+        if A > C:
+            print (f"{A}, {B},  Son los mayores") 
+        else: 
+            if A == C:
+                print (f"{A}, {B}, {C} son iguales") 
+            else: 
+                print (f"{C} Es el mayor")
+    else: 
+        if B > C: 
+            print (f"{B} es el mayor") 
+        else: 
+            if B == C : 
+                print (f"{B} y {C} son los mayores") 
+            else: 
+                print (f"{C} es el mayor") 
+print ("Fin del programa") 
+
