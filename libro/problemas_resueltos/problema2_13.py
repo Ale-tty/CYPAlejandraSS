704c01fe597767285e2410f138062109e925c2db
ale441 <alejandra.p.sanchez.s@gmail.com>
2019-10-14 23:51:23 -0500

    Se agrego el problema 2_13.py

--- libro/problemas_resueltos/problema2_13.py
+++ libro/problemas_resueltos/problema2_13.py
@@ -1,1 +1,20 @@
-
+MAT =int (input ("Introduce tu matricula "))
+CARR = str (input ("Introduce tu carrera ")) 
+SEM = int (input ("Inroduce tu semestre "))
+PROM = float (input ("Introduce tu promedio "))
+if CARR == economia : 
+    if (SEM >= 6) (PROM >= 8.8):
+        print (f"{MAT} de la carrera {CARR} estas aceptado")
+    else: 
+        print ("No seleccionado")
+elif CARR == computacion : 
+    if (SEM > 6) (PROM > 8.5): 
+        print (f"{MAT} de la carrera {CARR} estas aceptado")
+    else: 
+        print ("No seleccionado") 
+elif CARR == contabilidad : 
+    if (SEM > 5) (PROM > 8.5):
+        print (f"{MAT} de la carrera {CARR} estas aceptado") 
+    else: 
+        print ("No seleccionado") 
+print ("Fin del programa") 
